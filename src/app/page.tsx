dbfe38ce6aa45bde8406e640595930869a8cedb8
stjepans <ss85171@student.uni-lj.si>
2025-09-18 23:23:00 +0200

    first test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <h1 className="text-2xl text-center sm:text-3xl">Next.js</h1>
+        <h1 className="text-2xl text-center sm:text-3xl">Hello, this project is built using Next.js</h1>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://docs.sevalla.com/application-hosting"
